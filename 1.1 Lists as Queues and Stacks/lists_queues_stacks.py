2a1fe9536026ac3116e8329358701481cc6c2c53
sudharaD <sudharad7@gmail.com>
2022-12-29 17:56:44 +0530

    queues

--- 1.1 Lists as Queues and Stacks/lists_queues_stacks.py
+++ 1.1 Lists as Queues and Stacks/lists_queues_stacks.py
@@ -1,3 +1,4 @@
+# Stacks
 my_stack = []
 
 def push(stack, value):
@@ -11,3 +12,27 @@ push(my_stack, "b")
 push(my_stack, "c")
 
 print(my_stack)
+
+print(pop(my_stack))
+
+print(my_stack)
+
+print("--------------------------------------------")
+# Queues
+my_queue = []
+
+def enqueue(queue, value):
+    queue.append(value)
+
+def dequeue(queue):
+    return queue.pop(0)
+
+enqueue(my_queue, "a")
+enqueue(my_queue, "b")
+enqueue(my_queue, "c")
+
+print(my_queue)
+
+print(dequeue(my_queue))
+
+print(my_queue)
